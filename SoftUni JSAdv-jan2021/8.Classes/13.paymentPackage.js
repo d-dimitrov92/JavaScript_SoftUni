bc286d3b617a525a83b94766bc9ff80ed0a40ed1
d-dimitrov92 <eng.d.dimitrov92@gmail.com>
2021-02-16 22:34:02 +0200

    8.exercise

---
+++ SoftUni JSAdv-jan2021/8.Classes/13.paymentPackage.js
@@ -0,0 +1,135 @@
+// { describe } = require('mocha');
+// { assert } = require('chai');
+
+const { assert } = require("chai");
+
+class PaymentPackage {
+    constructor(name, value) {
+        this.name = name;
+        this.value = value;
+        this.VAT = 20;      // Default value    
+        this.active = true; // Default value
+    }
+
+    get name() {
+        return this._name;
+    }
+
+    set name(newValue) {
+        if (typeof newValue !== 'string') {
+            throw new Error('Name must be a non-empty string');
+        }
+        if (newValue.length === 0) {
+            throw new Error('Name must be a non-empty string');
+        }
+        this._name = newValue;
+    }
+
+    get value() {
+        return this._value;
+    }
+
+    set value(newValue) {
+        if (typeof newValue !== 'number') {
+            throw new Error('Value must be a non-negative number');
+        }
+        if (newValue < 0) {
+            throw new Error('Value must be a non-negative number');
+        }
+        this._value = newValue;
+    }
+
+    get VAT() {
+        return this._VAT;
+    }
+
+    set VAT(newValue) {
+        if (typeof newValue !== 'number') {
+            throw new Error('VAT must be a non-negative number');
+        }
+        if (newValue < 0) {
+            throw new Error('VAT must be a non-negative number');
+        }
+        this._VAT = newValue;
+    }
+
+    get active() {
+        return this._active;
+    }
+
+    set active(newValue) {
+        if (typeof newValue !== 'boolean') {
+            throw new Error('Active status must be a boolean');
+        }
+        this._active = newValue;
+    }
+
+    toString() {
+        const output = [
+            `Package: ${this.name}` + (this.active === false ? ' (inactive)' : ''),
+            `- Value (excl. VAT): ${this.value}`,
+            `- Value (VAT ${this.VAT}%): ${this.value * (1 + this.VAT / 100)}`
+        ];
+        return output.join('\n');
+    }
+}
+
+describe('PaymentPackage', () => {
+    it('constructor', () => {
+        let instance = new PaymentPackage('Name', 100);
+        assert.equal(instance._name, 'Name');
+        assert.equal(instance._value, 100);
+        assert.equal(instance._VAT, 20);
+        assert.equal(instance._active, true);
+    })
+    it('name', () => {
+        let instance = new PaymentPackage('Name', 100);
+        assert.equal(instance.name, 'Name');
+        instance.name = 'Pesho';
+        assert.throw(() => { instance.name = '' }, 'Name must be a non-empty string')
+        assert.throw(() => { instance.name = 2 }, 'Name must be a non-empty string')
+
+    })
+    it('value', () => {
+        let instance = new PaymentPackage('Name', 100);
+        assert.equal(instance.value, 100);
+        instance.value = 40;
+        assert.equal(instance.value, 40);
+        assert.throw(() => { instance.value = '2' }, 'Value must be a non-negative number')
+        assert.throw(() => { instance.value = -2 }, 'Value must be a non-negative number')
+        assert.doesNotThrow(() => { instance.value = 0 })
+    })
+    it('VAT', () => {
+        let instance = new PaymentPackage('Name', 100);
+        assert.equal(instance.VAT, 20);
+        instance.VAT = 40;
+        assert.equal(instance.VAT, 40);
+        assert.throw(() => { instance.VAT = -2 }, 'VAT must be a non-negative number')
+        assert.throw(() => { instance.VAT = '2' }, 'VAT must be a non-negative number')
+    })
+    it('active', () => {
+        let instance = new PaymentPackage('Name', 100);
+        assert.equal(instance.active, true);
+        instance.active = false;
+        assert.isFalse(instance.active);
+        assert.throw(() => { instance.active = 0 })
+    })
+    it('toString', () => {
+        let instance = new PaymentPackage('Name', 100);
+        function getString(name, value, VAT = 20, active = true) {
+            return [
+                `Package: ${name}` + (active === false ? ' (inactive)' : ''),
+                `- Value (excl. VAT): ${value}`,
+                `- Value (VAT ${VAT}%): ${value * (1 + VAT / 100)}`
+            ].join('\n');
+        }
+
+        assert.equal(instance.toString(), getString('Name', 100))
+        instance.active = false;
+        assert.equal(instance.toString(), getString('Name', 100, 20, false));
+        instance.name = 'Pesho'
+        assert.equal(instance.toString(), getString('Pesho', 100, 20, false));
+        instance.VAT = 200;
+        assert.equal(instance.toString(), getString('Pesho', 100, 200, false));
+    })
+})
---
+++ SoftUni JSAdv-jan2021/Exam Prep 1 - 12.02.2021/01. SoftUni Administration/index.js
@@ -0,0 +1,6 @@
+function solve() {
+    const addBtn = document.querySelector('.form-control>button');
+    addBtn.addEventListener('click', (e) => {
+        
+    })
+};
