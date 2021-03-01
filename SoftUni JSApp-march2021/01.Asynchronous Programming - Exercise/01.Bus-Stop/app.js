57aaee2de550838f566de89bc5d09a8e8bc68654
d-dimitrov92 <eng.d.dimitrov92@gmail.com>
2021-03-01 23:37:56 +0200

    JSApp01

--- SoftUni JSApp-march2021/01.Asynchronous Programming - Exercise/01.Bus-Stop/app.js
+++ SoftUni JSApp-march2021/01.Asynchronous Programming - Exercise/01.Bus-Stop/app.js
@@ -1,3 +1,26 @@
-function getInfo() {
-    console.log("TODO...");
+async function getInfo() {
+    const input = document.getElementById('stopId');
+    const id = input.value;
+
+    const url = 'http://localhost:3030/jsonstore/bus/businfo/' + id
+    try{
+        const ulElement = document.getElementById('buses');
+        ulElement.innerHTML = '';
+
+        const response = await fetch(url);
+        const data = await response.json();
+
+        document.getElementById('stopName').textContent = data.name;
+
+        Object.entries(data.buses).map(([bus, time]) =>{
+            const result = document.createElement('li');
+            result.textContent = `Bus ${bus} arrives in ${time} minutes`;
+
+            ulElement.appendChild(result);
+        });
+
+        input.value = '';
+    } catch(error){
+        document.getElementById('stopName').textContent = 'Error'
+    }
 }
