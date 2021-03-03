7c4259e11846ce3a0a69f9fba64e71a2f419e227
d-dimitrov92 <eng.d.dimitrov92@gmail.com>
2021-03-03 20:35:09 +0200

    JSApp01

--- SoftUni JSApp-march2021/01.Asynchronous Programming - Exercise/03.Forecaster/app.js
+++ SoftUni JSApp-march2021/01.Asynchronous Programming - Exercise/03.Forecaster/app.js
@@ -1,5 +1,16 @@
 function attachEvents() {
-    console.log("TODO...");
+    document.getElementById('submit').addEventListener('click', getWeather)
 }
 
+async function getWeather() {
+    const input = document.getElementById('location');
+    const location = input.value;
+    input.value = '';
+    const url = 'http://localhost:3030/jsonstore/forecaster/locations';
+
+    const response = await fetch(url);
+    const data = await response.json();
+    
+    return data;
+}
 attachEvents();
--- SoftUni JSApp-march2021/01.Asynchronous Programming - Exercise/06.Blog/app.js
+++ SoftUni JSApp-march2021/01.Asynchronous Programming - Exercise/06.Blog/app.js
@@ -1,5 +1,57 @@
 function attachEvents() {
-    console.log('TODO...');
+    document.getElementById('btnLoadPosts').addEventListener('click', getPosts);
+    document.getElementById('btnViewPost').addEventListener('click', displayPost);
 }
 
 attachEvents();
+
+async function getPosts() {
+    const select = document.getElementById('posts');
+    const url = 'http://localhost:3030/jsonstore/blog/posts'
+
+    let response = await fetch(url);
+    let data = await response.json();
+
+    Object.values(data).map(createOption).forEach(o => select.appendChild(o));
+}
+
+async function getCommentsByPostId(postId) {
+    const postsUrl = 'http://localhost:3030/jsonstore/blog/posts/' + postId
+    const commentsUrl = 'http://localhost:3030/jsonstore/blog/comments';
+    
+    const [postResponse, commentsResponse] = await Promise.all([
+        fetch(postsUrl),
+        fetch(commentsUrl)
+    ]);
+    
+    const postData = await postResponse.json();
+    const commentsData = await commentsResponse.json();
+
+    document.getElementById('post-title').textContent = postData.title;
+    document.getElementById('post-body').textContent = postData.body;
+
+    const comments = Object.values(commentsData).filter(c => c.postId == postId);
+
+    const commentsUl = document.getElementById('post-comments');
+    commentsUl.innerHTML = '';
+    comments.map(createComment).forEach(c => commentsUl.appendChild(c))
+}
+function createComment(c) {
+    const li = document.createElement('li');
+    li.textContent = c.text;
+    li.id = c.id;
+    return li;
+}
+
+function createOption(post) {
+    const result = document.createElement('option');
+    result.textContent = post.title;
+    result.value = post.id;
+
+    return result;
+}
+
+function displayPost() {
+    const postId = document.getElementById('posts').value;
+    getCommentsByPostId(postId);
+}
