d5bf07edebd49ed45c480fa2139954a7a7a8b955
d-dimitrov92 <eng.d.dimitrov92@gmail.com>
2021-03-17 01:06:36 +0200

    JSApp06

--- SoftUni JSApp-march2021/06. Client Side Rendering - Exercise/01.List-Towns/app.js
+++ SoftUni JSApp-march2021/06. Client Side Rendering - Exercise/01.List-Towns/app.js
@@ -1,4 +1,20 @@
 import { html, render } from '../../../node_modules/lit-html/lit-html.js';
 
-const listTemplate = () => html`
-`;
+document.getElementById('btnLoadTowns').addEventListener('click', getDataFromInput)
+const root = document.getElementById('root');
+
+function getDataFromInput(event) {
+    event.preventDefault();
+
+    const townsString = document.getElementById('towns').value;
+    const towns = townsString.split(',').map(x => x.trim());
+    const result = listTemplate(towns);
+
+    render(result, root)
+}
+
+
+const listTemplate = (data) => html`
+<ul>
+    ${data.map(t => html`<li>${t}</li>`)}
+</ul>`;
---
+++ SoftUni JSApp-march2021/06. Client Side Rendering - Exercise/02.HTTP-Status-Cats/app.js
@@ -0,0 +1,39 @@
+import { html, render } from '../../../node_modules/lit-html/lit-html.js';
+import { styleMap } from '../../../node_modules/lit-html/directives/style-map.js';
+import { cats as catsData } from './catSeeder.js';
+
+const cardTemplate = (data) => html`
+<li>
+    <img src="./images/${data.imageLocation}.jpg" width="250" height="250" alt="Card image cap">
+    <div class="info">
+        <button class="showBtn">Show status code</button>
+        <div class="status" style=${styleMap(data.info ? {} : {display: 'none'})} id=${data.id}>
+            <h4>Status Code: ${data.statusCode}</h4>
+            <p>${data.statusMessage}</p>
+        </div>
+    </div>
+</li>`;
+
+const main = document.getElementById('allCats');
+catsData.forEach(c => c.info = false);
+
+update();
+
+function update() {
+    const catsList = html`
+    <ul @click=${toggleInfo}>
+        ${catsData.map(c => cardTemplate(c))}
+    </ul>`;
+
+    render(catsList, main);
+}
+
+function toggleInfo(event) {
+    const elementId = event.target.parentNode.querySelector('.status').id;
+    const cat = catsData.find(c => c.id == elementId);
+    
+    if(event.target.className == 'showBtn'){
+        cat.info = !cat.info;
+        update();
+    }
+}
