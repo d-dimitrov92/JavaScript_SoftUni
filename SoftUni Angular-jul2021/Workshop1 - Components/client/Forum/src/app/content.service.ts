c2d9a3f523eb890725b1052434285799dac765e6
d-dimitrov92 <eng.d.dimitrov92@gmail.com>
2021-07-24 21:04:36 +0300

    workshop1_angular - components - forum

---
+++ SoftUni Angular-jul2021/Workshop1 - Components/client/Forum/src/app/content.service.ts
@@ -0,0 +1,22 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { IPost, ITheme } from './shared/interfaces';
+
+import { environment } from '../environments/environment';
+
+const API_URL = environment.apiURL;
+
+@Injectable()
+export class ContentService {
+
+  constructor(private http: HttpClient) { }
+
+  loadThemes() {
+    return this.http.get<ITheme[]>(`${API_URL}/themes`);
+  }
+
+  loadPosts(limit?: number) {
+    let query = limit ? `?limit=${limit}` : ''
+    return this.http.get<IPost[]>(`${API_URL}/posts${query}`);
+  }
+}
